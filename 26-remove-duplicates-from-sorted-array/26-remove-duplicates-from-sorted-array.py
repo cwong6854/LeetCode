1088f9ce932f93a8160aaf0c16a4b6c095571f9a
cwong6854 <96265081+cwong6854@users.noreply.github.com>
2022-01-10 13:55:12 -0800

    Time: 159 ms (12.24%), Space: 14.9 MB (46.52%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = 0
+        n = []
+        for i in range(len(nums)):
+            if nums[i] not in n:
+                n.append(nums[i])
+                nums[k] = nums[i]
+                k += 1
+            else:
+                continue
+        return k
