1ae399ebc76f53af54dd432953674aa3835df8cc
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-01-28 16:50:56 -0800

    Time: 74 ms (6.58%), Space: 13.4 MB (74.44%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        count = 0
+        count_s = 0
+        count_t = 0
+        unique = set(t)
+        total_letters = len(unique)
+        while count < total_letters:
+            for i in unique:
+                if i in s:
+                    for j in range(len(s)):
+                        if s[j] == i:
+                            count_s += 1
+                    for k in range(len(t)):
+                        if t[k] == i:
+                            count_t += 1
+                    if count_s == count_t:
+                        count_s = 0
+                        count_t = 0
+                        continue
+                    else:
+                        return i
+                else:
+                    return i
+                    
+            
