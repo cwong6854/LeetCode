b9a750271fb3c857647cccebc7a096a94ede1798
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-01-28 16:54:26 -0800

    Time: 20 ms (91.92%), Space: 13.6 MB (49.25%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -5,27 +5,32 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        count = 0
-        count_s = 0
-        count_t = 0
+        # count = 0
+        # count_s = 0
+        # count_t = 0
+        # unique = set(t)
+        # total_letters = len(unique)
+        # while count < total_letters:
+        #     for i in unique:
+        #         if i in s:
+        #             for j in range(len(s)):
+        #                 if s[j] == i:
+        #                     count_s += 1
+        #             for k in range(len(t)):
+        #                 if t[k] == i:
+        #                     count_t += 1
+        #             if count_s == count_t:
+        #                 count_s = 0
+        #                 count_t = 0
+        #                 continue
+        #             else:
+        #                 return i
+        #         else:
+        #             return i
         unique = set(t)
-        total_letters = len(unique)
-        while count < total_letters:
-            for i in unique:
-                if i in s:
-                    for j in range(len(s)):
-                        if s[j] == i:
-                            count_s += 1
-                    for k in range(len(t)):
-                        if t[k] == i:
-                            count_t += 1
-                    if count_s == count_t:
-                        count_s = 0
-                        count_t = 0
-                        continue
-                    else:
-                        return i
-                else:
-                    return i
+        for i in unique:
+            if s.count(i) < t.count(i):
+                return i
+        return None
                     
             
