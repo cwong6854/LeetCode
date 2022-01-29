88825dd73283c6a06f51bc7d9601d4a38d6c9ece
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-01-28 16:11:20 -0800

    Time: 20 ms (61.30%), Space: 13.9 MB (8.83%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -4,5 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        lst = s.split()
-        return len(lst[-1])
+        # lst = s.split()
+        # return len(lst[-1])
+        word = ""
+        if len(s.split()) == 1:
+            word = s.split()
+            return len(word[0])
+        else:
+            for i in range(len(s) - 1, -1, -1):
+                if s[i] != " ":
+                    word += s[i]
+                elif len(word) == 0:
+                    continue
+                else:
+                    return len(word)
