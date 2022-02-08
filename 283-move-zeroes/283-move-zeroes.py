8425fbe66088b161a380fe3df5f0c8d1dc9b1a2d
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-02-08 10:45:07 -0800

    Time: 1675 ms (8.00%), Space: 14.5 MB (85.47%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,15 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        count = 0
-        count2 = 0
         for i in nums:
             if i == 0:
-                count += 1
-                count2 += 1
-        while count > 0:
-            nums.remove(0)
-            nums.append(0)
-            count -= 1
+                nums.append(0)
+                nums.remove(i)
         
                 
