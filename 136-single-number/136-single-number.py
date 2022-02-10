70c476526baf7408c139e43a6dd7d40aae0194ea
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-02-10 08:42:50 -0800

    Time: 4169 ms (11.00%), Space: 16.2 MB (29.25%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -4,18 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        numb = set(nums)
-        count = 0
-        currnumb = 0
-        for i in numb:
-            if count == 1:
-                return currnumb
-            else:
-                count = 0
-                currnumb = i
-                for j in nums:
-                    if i == j:
-                        count += 1
-        if count == 1:
-            return currnumb
+        # numb = set(nums)
+        # count = 0
+        # currnumb = 0
+        # for i in numb:
+        #     if count == 1:
+        #         return currnumb
+        #     else:
+        #         count = 0
+        #         currnumb = i
+        #         for j in nums:
+        #             if i == j:
+        #                 count += 1
+        # if count == 1:
+        #     return currnumb
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            for i in set(nums):
+                counts = nums.count(i)
+                if counts == 1:
+                    return i
                     
