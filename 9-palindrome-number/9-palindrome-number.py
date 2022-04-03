283efc6b604d21d209f42b39ced58c781132aed8
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-04-02 22:12:12 -0700

    Time: 90 ms (40.41%), Space: 13.5 MB (12.66%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -3,23 +3,7 @@ class Solution(object):
         """
         :type x: int
         :rtype: bool
-        """
-        # length = len(str(x))
-        # if length == 0:
-        #     return False
-        # else:
-        #     x_str = str(x)
-        #     lst = []
-        #     for i in range(len(x_str)):
-        #         lst.append(x_str[i])
-        #     print(lst)
-        #     if lst == list(reversed(lst)):
-        #         return True
-        #     else:
-        #         return False
-            # if length % 2 == 0:
-            #     for i in range(int(length / 2), ):
-                    
+        """        
         x = str(x)
         if list(x) == list(reversed(x)):
             return True
