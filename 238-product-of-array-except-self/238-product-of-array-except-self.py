9b24869e6f4596750ef5729d89e5de0f41cba894
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-08-16 12:28:22 -0700

    Time: 175 ms (99.51%), Space: 20.6 MB (71.91%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        # runtime => O(N^2)
+        # answer = []
+        # index = 0
+        # total = 1
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i == j:
+        #             continue
+        #         else:
+        #             total *= nums[j]
+        #     answer.append(total)
+        #     total = 1
+        # return answer
+        
+        nums_length = len(nums)
+        productExceptSelf = [1] * nums_length  
+        # prefix -> multiplying elements from the left
+        for i in range(1, nums_length):
+            productExceptSelf[i] = productExceptSelf[i - 1] * nums[i - 1]
+        # suffix - multiplying elements from the right
+        # since most right element has no right element, right starts at value of 1
+        right = 1
+        for i in range(nums_length - 1, -1, -1):
+            productExceptSelf[i] = productExceptSelf[i] * right
+            right = right * nums[i]
+
+        return productExceptSelf
+            
+
+            
+            
+        
