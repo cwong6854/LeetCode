5c9e9c79c167463a3bdc5882c37b5521cef5127c
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-08-25 23:41:06 -0700

    Time: 291 ms (54.90%), Space: 20.4 MB (77.96%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -19,20 +19,53 @@ class Solution(object):
         #     total = 1
         # return answer
         
-        nums_length = len(nums)
-        productExceptSelf = [1] * nums_length  
-        # prefix -> multiplying elements from the left
-        for i in range(1, nums_length):
-            productExceptSelf[i] = productExceptSelf[i - 1] * nums[i - 1]
-        # suffix - multiplying elements from the right
-        # since most right element has no right element, right starts at value of 1
-        right = 1
-        for i in range(nums_length - 1, -1, -1):
-            productExceptSelf[i] = productExceptSelf[i] * right
-            right = right * nums[i]
+#         nums_length = len(nums)
+#         productExceptSelf = [1] * nums_length  
+#         # prefix -> multiplying elements from the left
+#         for i in range(1, nums_length):
+#             productExceptSelf[i] = productExceptSelf[i - 1] * nums[i - 1]
+#         # suffix - multiplying elements from the right
+#         # since most right element has no right element, right starts at value of 1
+#         right = 1
+#         for i in range(nums_length - 1, -1, -1):
+#             productExceptSelf[i] = productExceptSelf[i] * right
+#             right = right * nums[i]
 
-        return productExceptSelf
+#         return productExceptSelf
             
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        arr = [1] * len(nums)
+        length = len(nums)
+        prefix = 1
+        for i in range(1, length):
+            arr[i] = nums[i - 1] * arr[i - 1]
+            prefix = nums[i - 1]
+        right = 1
+        for i in range(length - 1, -1, -1):
+            arr[i] = right * arr[i]
+            right *= nums[i]
+        return arr
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
             
             
