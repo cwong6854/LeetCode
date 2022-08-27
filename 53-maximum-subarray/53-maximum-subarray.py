99e1fb4173d6dfa9e5b677a27472b19e6e1f2419
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-08-27 12:19:55 -0700

    Time: 2024 ms (5.01%), Space: 25.5 MB (90.55%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,28 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        # at minimum -> O(n^2)
-        # largestSum = []
-        # numsLength = len(nums)
-        # if numsLength == 1:
-        #     return nums[0]
-        # for i in range(len(nums)):
-        #     total = 0
-        #     for j in range(i, numsLength):
-        #         total += nums[j]
-        #         if len(largestSum) == 0:
-        #             largestSum.append(total)
-        #         else:
-        #             if total > largestSum[0]:
-        #                 largestSum[0] = total
-        # return largestSum[0]
-
-        largestSum = nums[0]
+        # subarray could be [1]
+        maxSum = nums[0]
         curSum = 0
+        # [-2, 1] -> -1
+        length = len(nums)
         for i in range(len(nums)):
             if curSum < 0:
                 curSum = 0
-            curSum += nums[i]
-            largestSum = max(curSum, largestSum)
-        return largestSum
+            curSum = curSum + nums[i]
+            maxSum = max(curSum, maxSum)
+            print(curSum, maxSum)
+        return maxSum
+        
