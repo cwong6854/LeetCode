16e9bc1d0635c6294463037807bcd283f7fd0e3b
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-08-27 14:50:41 -0700

    Time: 1094 ms (19.50%), Space: 25.6 MB (73.56%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # subarray could be [1]
         maxSum = nums[0]
         curSum = 0
-        # [-2, 1] -> -1
         length = len(nums)
         for i in range(len(nums)):
             if curSum < 0:
                 curSum = 0
             curSum = curSum + nums[i]
             maxSum = max(curSum, maxSum)
-            print(curSum, maxSum)
+            print(curSum)
         return maxSum
         
