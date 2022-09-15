7a2c253ecb624804be29e05fa1b6b82ca832f726
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-09-15 09:36:59 -0700

    Time: 159 ms (5.01%), Space: 13.8 MB (76.87%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -6,6 +6,12 @@ class Solution(object):
         """
         # [2, 3, -2, 4]
         # -n < 0 < n
+        
+        #edge cases
+        # all positives -> whole array itself
+        # negatives -> don't want to return a big negative value -> keep track of min value
+        # 0: can bring the rest of our products to 0
+        
         largestProduct = nums[0]
         maxProduct = 1
         minProduct = 1
@@ -14,7 +20,8 @@ class Solution(object):
             maxTemp = maxProduct
             maxProduct = max(n, maxProduct * n, minProduct * n)
             minProduct = min(n, maxTemp * n, minProduct * n)
-            largestProduct = max(maxProduct, largestProduct)
+            largestProduct = max(largestProduct, maxProduct)
+            print(largestProduct, maxProduct, minProduct)
         return largestProduct
             
         
