90427711982daa6b7239ab9d6976d833a2ea13c1
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-09-15 09:25:58 -0700

    Time: 121 ms (21.94%), Space: 13.6 MB (97.36%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -4,25 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-            
-            
-    #         largestProduct -> the number itself
-    #         case with all positives: the whole array itself
-    #         case with negatives: 
-    #         case with 0 in the array: could cause the rest of products to become 0
-    #         need to store the previous maximum number, would also need to store the minimum number, to compare with max
-
-    #         PV -> -x < 0 < x
-    
+        # [2, 3, -2, 4]
+        # -n < 0 < n
         largestProduct = nums[0]
         maxProduct = 1
         minProduct = 1
-        
-        # 1 * 2 = 2 -> maxProduct = 2, minProduct = 2
-        # 2 * 3 = 6 -> maxProduct = 6, minProduct = 2
-        # 6 * -2 = 12 -> maxProduct = 6, minProduct = -12
-        # 6 * 4 = 24 -> error
         
         for n in nums:
             maxTemp = maxProduct
@@ -30,4 +16,6 @@ class Solution(object):
             minProduct = min(n, maxTemp * n, minProduct * n)
             largestProduct = max(maxProduct, largestProduct)
         return largestProduct
+            
+        
         
