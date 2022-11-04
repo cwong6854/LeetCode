4828afc800a7a7a544a01749cd4819b50c1d2429
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-11-03 23:56:03 -0700

    Time: 14 ms (95.08%), Space: 13.4 MB (35.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,19 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # if n = 1, then only one possible way
-        if n == 1:
-            return 1
-        # if n = 2, then there's two possible ways
-        if n == 2:
-            return 2
+        one, two = 1, 1
         
-        all_ways = 0
-        one_step = 1
-        two_step = 2
-        
-        for i in range(2, n):
-            all_ways = one_step + two_step
-            one_step = two_step
-            two_step = all_ways
-        return all_ways
+        for i in range(n - 1):
+            one, two = one + two, one
+        return one
