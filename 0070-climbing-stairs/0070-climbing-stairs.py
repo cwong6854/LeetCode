c2104ab442735d71e3e96f28604dded3d825a3c2
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-11-06 12:34:23 -0800

    Time: 25 ms (66.69%), Space: 13.5 MB (35.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,8 +4,18 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        one, two = 1, 1
-        
-        for i in range(n - 1):
-            one, two = one + two, one
-        return one
+        one, two = 1, 2
+        all_ways = 0
+        # 1 <= n <= 45
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        for i in range(3, n+1):
+            all_ways = one + two
+            one = two
+            two = all_ways
+            print(all_ways)
+        return all_ways
+            
+            
