655f8e770a2cd7bfb75c503d01997b9f6110ea10
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-11-06 12:48:42 -0800

    Time: 32 ms (36.32%), Space: 13.7 MB (12.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,17 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        # at n >= 3 --> want to store our steps and track all distinct ways
-        one = 1
-        two = 2
-        all_ways = 0
-        for i in range(3, n+1):
-            all_ways = one + two
-            one = two
-            two = all_ways
-        return all_ways
+        one, two = 1, 1
+        for i in range(n-1):
+            one, two = one+two, one
+        return one
             
