47cf2ed4e9131965c1800bc1b687cc1a94858dda
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-11-12 13:39:37 -0800

    Time: 130 ms (62.11%), Space: 17.3 MB (59.88%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,14 +4,27 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        res = []
-        for i in range(n+1):
-            arr = []
-            while i > 0:
-                arr.append(i % 2)
-                i = i // 2
-            res.append(sum(arr))
-        return res
+        # runtime: o(nlogn)
+        
+        # res = []
+        # for i in range(n+1):
+        #     arr = []
+        #     while i > 0:
+        #         arr.append(i % 2)
+        #         i = i // 2
+        #     res.append(sum(arr))
+        # return res
+        
+        dp = [0] * (n+1)
+        x = 1
+        # loop through each index until n
+        for i in range(1, n + 1):
+            if x * 2 == i:
+                x = x*2
+            dp[i] = 1 + dp[i-x]
+        return dp
+        
+        
     
             
         
