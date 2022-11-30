24c9b3e004c9d585e1759d325ad79c3eb8f02142
Curtis Wong <96265081+cwong6854@users.noreply.github.com>
2022-11-30 10:00:57 -0800

    Time: 1250 ms (49.45%), Space: 24.8 MB (32.57%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -7,12 +7,17 @@ class Solution(object):
         """
         dic = {}
         for i in range(len(nums)):
-            if nums[i] not in dic:
-                dic[nums[i]] = i
-                print(i)
-            else:
-                if abs(dic[nums[i]] - i) <= k:
-                    return True
-                else:
-                    dic[nums[i]] = i
+            if nums[i] in dic and abs(dic[nums[i]] - i) <= k:
+                return True
+            dic[nums[i]] = i
         return False
+            
+            
+#             if nums[i] not in dic:
+#                 dic[nums[i]] = i
+#             else:
+#                 if abs(dic[nums[i]] - i) <= k:
+#                     return True
+#                 else:
+#                     dic[nums[i]] = i
+#         return False
